2693039d383dc0672d8fc6c58712e8dfb9744d2a
syun560 <bajikoji@gmail.com>
2019-02-07 21:21:10 +0900

    時間制限を実装

--- BaseScene.cpp
+++ BaseScene.cpp
@@ -1,13 +1,32 @@
 #include "BaseScene.h"
 
 BaseScene::BaseScene() {
-
+	time = 60 * 5; // 60FPSの場合5秒
+	state = PLAYING;
 }
 
 int BaseScene::Update() {
+	if (state == PLAYING) time--;
+	if (time == 0) {
+		state = CLEARED;
+	}
 	return 0;
 }
 
-void BaseScene::Draw() {
+void BaseScene::Miss() {
+	state = MISS;
+}
 
+void BaseScene::Draw() {
+	// カウントダウン
+	DrawFormatString(0, 0, MyDx::WHITE, "%d", time);
+	if (state != PLAYING) {
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 122);
+		DrawBox(50,50,MyDx::FMX-50,MyDx::FMY-50,MyDx::WHITE,TRUE);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+		if(state == CLEARED) DrawFormatString(100, 200, MyDx::GREEN, "Cleared");
+		else if(state == MISS) DrawFormatString(100, 200, MyDx::RED, "MISS");
+		DrawFormatString(100, 300, MyDx::WHITE, "Press ESC to title");
+		DrawFormatString(100, 320, MyDx::WHITE, "Press R to retry");
+	}
 }
--- BaseScene.h
+++ BaseScene.h
@@ -2,8 +2,17 @@
 #include "MyDx.h"
 
 class BaseScene {
+	enum State {
+		PLAYING,
+		PAUSE,
+		CLEARED,
+		MISS,
+	};
+	int time;
+	State state;
 public:
 	BaseScene();
+	void Miss();
 	virtual int Update();
 	virtual void Draw();
 };
